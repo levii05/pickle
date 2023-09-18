8a5d7b69568245c73eb154ac22c45145e58fd028
belle <bell.levi2406@gmail.com>
2023-09-18 11:23:43 +0300

    12

--- screen.py
+++ screen.py
@@ -1,6 +1,11 @@
 import pygame
 import consts
 
-screen = pygame.display.set_mode(consts.SCREEN_WIDTH, consts.SCREEN_LENGTH)
+def screen_size():
+    pygame.init()
+    screen = pygame.display.set_mode((consts.SCREEN_WIDTH, consts.SCREEN_LENGTH))
 
-screen.fill((225,0,0))
+def screen_color():
+    screen.fill(consts.BACKGROUND_COLOR)
+
+# def random_bush():
